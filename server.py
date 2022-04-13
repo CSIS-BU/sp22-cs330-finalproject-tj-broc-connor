d1781bcc2e7ed2039a709675ea53c4b41da7bfdd
tcaron1024 <60328069+tcaron1024@users.noreply.github.com>
2022-04-13 16:26:53 -0500

    setting up basic python server code

---
+++ server.py
@@ -0,0 +1,28 @@
+import sys
+import socket
+
+RECV_BUFFER_SIZE = 2048
+QUEUE_LENGTH = 10
+
+def server(server_port):
+    # create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+        #bind socket to port
+        serversocket.bind((",server_port))
+        serversocket.listen(QUEUE_LENGTH)
+    
+        while True:
+            (clientsocket, address) = serversocket.accept
+            # FUNCTION TO RUN GAME LOOP HERE
+    pass
+
+def main():
+    # make sure they enter server port
+    if len(sys.argv) != 2:
+        sys.exit("Please enter the port to run the server on")
+    server_port = int(sys.argv[1])
+    server(server_port)
+
+if__name__ == "__main__":
+    main()
+        
